0682d8957eebd2ec370f63149d08b610c292b33f
DimaArsyonov <73301369+DimaArsyonov@users.noreply.github.com>
2020-12-06 17:04:41 +0300

    -amend

---
+++ src/main/java/lab4/lab4/Division.java
@@ -0,0 +1,24 @@
+package lab4.lab4;
+
+public class Division {
+
+	private long idDiv;
+	private String title;
+	
+	public void setIdDiv(long id) {
+		this.idDiv = id;
+	}
+	
+	public long getIdDiv() {
+        return idDiv;
+    }
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getTitle() {
+        return title;
+    }
+	
+}
